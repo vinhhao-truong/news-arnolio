d18fdfbbeb70a2abb0198a5e9167579163363a4a
arnold-tr <vinhhao.truong.52@gmail.com>
2023-06-06 23:26:26 +1000

    fix invalid meta

--- app/[category]/layout.tsx
+++ app/[category]/layout.tsx
@@ -2,21 +2,21 @@ import ReactProps from "@/lib/interfaces/ReactProp";
 import { capitaliseFirst } from "@/lib/utils/format/formatString";
 import { Metadata } from "next";
 
-type GenerateMetadataProps = {
+interface GenerateMetadataProps {
   params: { category: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+}
 export async function generateMetadata({
   params,
-  searchParams,
 }: GenerateMetadataProps): Promise<Metadata> {
   return {
     title: capitaliseFirst(params.category) + " | Arnolio News",
   };
 }
 
-const CategoryLayout: React.FC<ReactProps> = ({ children }) => {
+export default function CategoryLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
   return <>{children}</>;
-};
-
-export default CategoryLayout;
+}
